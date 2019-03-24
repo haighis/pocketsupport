19e09ca73704bc96938159a40a61eba659079333
John Haigh <haighis@gmail.com>
2019-03-24 10:53:21 -0400

    Cleanup for demo

--- menu.js
+++ menu.js
@@ -4,8 +4,15 @@ const app = electron.app
 
 const template = [
   {
-    label: 'Edit',
+    label: 'Edit', // change this in demo
     submenu: [
+      // { // add this in demo
+      //         label: 'Toggle Developer Tools',
+      //         accelerator: process.platform === 'darwin' ? 'Alt+Command+I' : 'Ctrl+Shift+I',
+      //         click (item, focusedWindow) {
+      //           if (focusedWindow) focusedWindow.webContents.toggleDevTools()
+      //         }
+      //       },
       {
         role: 'undo'
       },
@@ -35,54 +42,55 @@ const template = [
       }
     ]
   },
-  {
-    label: 'View',
-    submenu: [
-      {
-        label: 'Reload',
-        accelerator: 'CmdOrCtrl+R',
-        click (item, focusedWindow) {
-          if (focusedWindow) focusedWindow.reload()
-        }
-      },
-      {
-        label: 'Toggle Developer Tools',
-        accelerator: process.platform === 'darwin' ? 'Alt+Command+I' : 'Ctrl+Shift+I',
-        click (item, focusedWindow) {
-          if (focusedWindow) focusedWindow.webContents.toggleDevTools()
-        }
-      },
-      {
-        type: 'separator'
-      },
-      {
-        role: 'resetzoom'
-      },
-      {
-        role: 'zoomin'
-      },
-      {
-        role: 'zoomout'
-      },
-      {
-        type: 'separator'
-      },
-      {
-        role: 'togglefullscreen'
-      }
-    ]
-  },
-  {
-    role: 'window',
-    submenu: [
-      {
-        role: 'minimize'
-      },
-      {
-        role: 'close'
-      }
-    ]
-  },
+  // Uncomment this in demo
+  // {
+  //   label: 'View',
+  //   submenu: [
+  //     {
+  //       label: 'Reload',
+  //       accelerator: 'CmdOrCtrl+R',
+  //       click (item, focusedWindow) {
+  //         if (focusedWindow) focusedWindow.reload()
+  //       }
+  //     },
+  //     {
+  //       label: 'Toggle Developer Tools',
+  //       accelerator: process.platform === 'darwin' ? 'Alt+Command+I' : 'Ctrl+Shift+I',
+  //       click (item, focusedWindow) {
+  //         if (focusedWindow) focusedWindow.webContents.toggleDevTools()
+  //       }
+  //     },
+  //     {
+  //       type: 'separator'
+  //     },
+  //     {
+  //       role: 'resetzoom'
+  //     },
+  //     {
+  //       role: 'zoomin'
+  //     },
+  //     {
+  //       role: 'zoomout'
+  //     },
+  //     {
+  //       type: 'separator'
+  //     },
+  //     {
+  //       role: 'togglefullscreen'
+  //     }
+  //   ]
+  // },
+  // {
+  //   role: 'window',
+  //   submenu: [
+  //     {
+  //       role: 'minimize'
+  //     },
+  //     {
+  //       role: 'close'
+  //     }
+  //   ]
+  // },
   {
     role: 'help',
     submenu: [
@@ -147,29 +155,29 @@ if (process.platform === 'darwin') {
     }
   )
   // Window menu.
-  template[3].submenu = [
-    {
-      label: 'Close',
-      accelerator: 'CmdOrCtrl+W',
-      role: 'close'
-    },
-    {
-      label: 'Minimize',
-      accelerator: 'CmdOrCtrl+M',
-      role: 'minimize'
-    },
-    {
-      label: 'Zoom',
-      role: 'zoom'
-    },
-    {
-      type: 'separator'
-    },
-    {
-      label: 'Bring All to Front',
-      role: 'front'
-    }
-  ]
+  // template[3].submenu = [
+  //   {
+  //     label: 'Close',
+  //     accelerator: 'CmdOrCtrl+W',
+  //     role: 'close'
+  //   },
+  //   {
+  //     label: 'Minimize',
+  //     accelerator: 'CmdOrCtrl+M',
+  //     role: 'minimize'
+  //   },
+  //   {
+  //     label: 'Zoom',
+  //     role: 'zoom'
+  //   },
+  //   {
+  //     type: 'separator'
+  //   },
+  //   {
+  //     label: 'Bring All to Front',
+  //     role: 'front'
+  //   }
+  // ]
 }
 
 const menu = Menu.buildFromTemplate(template)
