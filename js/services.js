a96a779f2f1ce3f6f3facd03b199feaae7f727ae
John Haigh <haighis@gmail.com>
2019-03-24 10:01:01 -0400

    Cleanup for demo

--- js/services.js
+++ js/services.js
@@ -1,7 +1,5 @@
 angular.module('ngApp.services', [])
 .factory('NEDBService',function(){
-  
-  //var Datastore = require('nedb');
   var Datastore = require('nedb-promises');
   var electron = require('electron');
   
@@ -12,66 +10,25 @@ angular.module('ngApp.services', [])
     bootstrap : function(){
       
       var app = electron.remote.app;
-      //var userData = app.getAppPath('userData'); 
       var userData = app.getAppPath('userData'); 
-      console.log('in boostrap file path is ', userData)
-     db.customers = new Datastore({
-        filename: '../db/customers.db', // provide a path to the database file 
-        ///Users/johnhaigh/Projects/icmapp/pocketsupport/
-        autoload: true, // automatically load the database
-        timestampData: true // automatically add and manage the fields createdAt and updatedAt
-      });
-
-      db.orders = new Datastore({
-        //filename:  '/Users/johnhaigh/Projects/icmapp/pocketsupport/orders.db', // provide a path to the database file 
-        filename:  userData + '../db/orders.db', // provide a path to the database file 
+      db.support = new Datastore({
+        filename:  userData + '../db/support.db', // provide a path to the database file 
         autoload: true, // automatically load the database
         timestampData: true // automatically add and manage the fields createdAt and updatedAt
       });      
       return db;
     },
-    test : function(){
-        
-
-    },
     close : function(){
     },
     find : function(table,offset,limit){
       var db = this.bootstrap();
-      // db[table].find({}).sort({
-      //   updatedAt: -1
-      // }).exec(function(err, items) {
-      //   console.log('in find ', items)
-      //   if (err) {
-      //     return null;
-      //   }
-        
-      // }).then(function(results){
-      //   console.log('in find 2', results)
-      //   return results;
-      // });
       return db[table].find({}).sort({
         updatedAt: -1
       });
-      // .exec(function(err, items) {
-      //   console.log('in find ', items)
-      //   if (err) {
-      //     return null;
-      //   }
-        
-      // }).then(function(results){
-      //   console.log('in find 2', results)
-      //   return results;
-      // });
-
     },
    insert: function(table,item){
      var db = this.bootstrap();
       db[table].insert(item, function(err, item) {
-        console.log('insert ', item._id)
-        
-        //var test = get('orders', item._id)
-        //console.log('get item after create ', test)
         if (err) {
           return null;
         }
@@ -94,57 +51,6 @@ angular.module('ngApp.services', [])
         if (err) return null;
         return 1;
       });
-
    }
   }
-}) 
-.factory('DBService',function(MongooseService){
-    
-    this.pageSize = 10;
-    this.offset = 0;
-    this.limit = 10;
-    var self = this;    
-    
-    function size(table){
-
-    }
-    function find(table,offset,limit){
-        return NEDBService.find(table,offset,limit);
-    }
-    return {
-      bootstrap : function(info){
-        NEDBService.bootstrap();
-        //NEDBService.test();
-      },
-      getPager: async function(table,pageSize)
-      {
-          
-          self.pageSize = pageSize;
-          self.limit = self.pageSize;
-          self.size = await size();
-          var o =  {
-              
-              initialPage:function(){
-
-                return find(table,self.offset,self.limit);
-              },
-              prevPage:function(){
-                if(self.offset > 0 )
-                { 
-                  self.offset -= self.pageSize;
-                } 
-                 return find(table,self.offset,self.limit);         
-              },
-              nextPage:function(){
-                  if(self.offset  + self.limit <= self.size )
-                  {  
-                    self.offset +=  self.pageSize;
-                  }
-                  return find(table,self.offset,self.limit);  
-              }
-          }
-          return o;
-          
-      }        
-    }
 });
